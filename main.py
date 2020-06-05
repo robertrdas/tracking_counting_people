d26b6876331dc57cfebaa02014643b60d25bb851
robert_rdas <robert201r.d.a.s@hotmail.com>
2020-06-05 10:20:42 -0300

    [CHANGELOG]
    Prototipo de tracking das pessoas

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
-import numpy as np
 import cv2
-
+import numpy as np
 
 cap = cv2.VideoCapture("768x576.avi")
+frame_width = int( cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 
-while(cap.isOpened()):
-	ret, frame = cap.read()
+frame_height =int( cap.get( cv2.CAP_PROP_FRAME_HEIGHT))
 
-	if ret==False:
-		break
-	cv2.imshow("frame",frame)
+fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
 
-	if cv2.waitKey(1) & 0xFF == ord("q"):
-		break
+out = cv2.VideoWriter("output.avi", fourcc, 5.0, (1280,720))
 
-cap.release()
+ret, frame1 = cap.read()
+ret, frame2 = cap.read()
+print(frame1.shape)
+while cap.isOpened():
+    diff = cv2.absdiff(frame1, frame2)
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+    dilated = cv2.dilate(thresh, None, iterations=3)
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        (x, y, w, h) = cv2.boundingRect(contour)
+
+        if cv2.contourArea(contour) < 900:
+            continue
+        cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame1, "Status: {}".format('Movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
+                    1, (0, 0, 255), 3)
+    #cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
+
+    image = cv2.resize(frame1, (1280,720))
+    out.write(image)
+    cv2.imshow("feed", frame1)
+    frame1 = frame2
+    ret, frame2 = cap.read()
+
+    if cv2.waitKey(40) == 27:
+        break
+
 cv2.destroyAllWindows()
+cap.release()
+out.release()
